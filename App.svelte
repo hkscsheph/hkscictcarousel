bd7b290ec84efcb3b1c2a6808319eb48ef30d9f4
hkscsheph <90292198+hkscsheph@users.noreply.github.com>
2021-10-27 09:59:12 +0800

    50vh

--- App.svelte
+++ App.svelte
@@ -8,9 +8,8 @@
     font-family: sans-serif;
     text-align: center;
   }
-  .slide-content {
-    width: 50vh;
-    aspect-ratio: 1/1;
+  img {
+    height: 50vh;
   }
 </style>
 
@@ -22,16 +21,16 @@
   <br>
   <Carousel perPage="1">
     <div class="slide-content">
-      <img alt="1" src="https://static.wixstatic.com/media/038890_c00c8a1dbe0c4b22807c6ff749bb54fa~mv2.png/v1/fill/w_1349,h_611,al_c,q_90,usm_0.66_1.00_0.01/038890_c00c8a1dbe0c4b22807c6ff749bb54fa~mv2.webp" height="100%" />
+      <img alt="1" src="https://static.wixstatic.com/media/038890_07ea1f5cc2b94873b3408831cf195a4c~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_07ea1f5cc2b94873b3408831cf195a4c~mv2.webp"/>
     </div>
     <div class="slide-content">
-      <img alt="1" src="https://static.wixstatic.com/media/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.webp" height="100%" />
+      <img alt="2" src="https://static.wixstatic.com/media/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_e6f6851b4d0f4934b17dcf28a29674a8~mv2.webp"/>
     </div>
     <div class="slide-content">
-      <img alt="1" src="https://static.wixstatic.com/media/038890_458d7857e21747938575c3abe681bebc~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_458d7857e21747938575c3abe681bebc~mv2.webp" height="100%" />
+      <img alt="3" src="https://static.wixstatic.com/media/038890_458d7857e21747938575c3abe681bebc~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_458d7857e21747938575c3abe681bebc~mv2.webp"/>
     </div>
     <div class="slide-content">
-      <img alt="1" src="https://static.wixstatic.com/media/038890_38ef73d9d3f548068d3a235f59a626d0~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_38ef73d9d3f548068d3a235f59a626d0~mv2.webp" height="100%" />
+      <img alt="4" src="https://static.wixstatic.com/media/038890_38ef73d9d3f548068d3a235f59a626d0~mv2.jpg/v1/fill/w_789,h_526,al_c,q_85,usm_0.66_1.00_0.01/038890_38ef73d9d3f548068d3a235f59a626d0~mv2.webp"/>
     </div>
   </Carousel>
 
